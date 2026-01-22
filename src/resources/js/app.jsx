95136a608eb467074bab3c8f2bacbbb046fdb1de
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-01-22 12:56:40 +0800

    Landing Page init

--- src/resources/js/app.jsx
+++ src/resources/js/app.jsx
@@ -1,17 +1,9 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-function App() {
-    return (
-        <div className="container mt-5">
-            <h1 className="text-primary">React is working</h1>
-        </div>
-    );
-}
+import "./bootstrap";
+import Landing from "./pages/Landing";
 
 const el = document.getElementById("app");
 
 if (el) {
-    createRoot(el).render(<App />);
+    createRoot(el).render(<Landing />);
 }
---
+++ src/resources/js/pages/Landing.jsx
@@ -0,0 +1,9 @@
+function Landing() {
+    return (
+        <h1 style={{ color: "red" }}>
+            Landing page is rendering
+        </h1>
+    );
+}
+
+export default Landing;
