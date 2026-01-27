30d1c6d31fb767ccde9e5de924b9801945187d1d
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-01-27 16:14:49 +0800

    User Controller ini and a simple index function to retrieve users data

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::select('id', 'name', 'email', 'created_at')
+            ->orderBy('created_at', 'desc')
+            ->get();
+    }
+}
