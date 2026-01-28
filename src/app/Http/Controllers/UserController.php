e3b40f9c214aee49022fa2940681085157ab6ef7
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-01-28 21:29:24 +0800

    Register Function   - validate   - table data   - response

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -4,9 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name'=> 'required|string|min:3|max:50',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'user' => $user,
+        ], 201);
+    }
+
     public function index()
     {
         return User::select('id', 'name', 'email', 'created_at')
