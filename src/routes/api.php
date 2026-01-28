875ea86d1e44253f259058063ca0d7342b5d89d2
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-01-28 21:30:36 +0800

    api post request uri

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::get('/users', [UserController::class, 'index']);
+
+Route::post('/register', [UserController::class, 'register']);
