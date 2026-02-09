f3ef803652987037a1fa9b2ddd55ee2a675568ab
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-02-09 12:35:12 +0800

    components init and header component init

---
+++ src/resources/js/components/heading.jsx
@@ -0,0 +1,12 @@
+function Headers() {
+
+return (
+        <div className="bg-black px-5 py-4 text-center">
+            <div className="text-center text-md-start fs-2 fw-bold">
+                Workout Tracker
+            </div>
+        </div>
+    )
+}
+
+export default Headers;
