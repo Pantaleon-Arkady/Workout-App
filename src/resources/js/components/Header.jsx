c4a5337352e76d44daa89defcb93fc0287033e4a
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-02-16 10:37:54 +0800

    right feature of sign up toggle

--- src/resources/js/components/Header.jsx
+++ src/resources/js/components/Header.jsx
@@ -1,10 +1,20 @@
-function Headers() {
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+
+function Headers({ rightFeature }) {
+
+const {user, logout} = useAuth();
 
 return (
-        <div className="bg-black px-5 py-4 text-center">
+        <div className="bg-black px-5 py-4 text-center d-flex flex-row justify-content-between">
             <div className="text-center text-md-start fs-2 fw-bold">
                 Workout Tracker
             </div>
+            {rightFeature?
+                (<>SignUp</>)
+                :
+                (<>About Us</>)
+            }
         </div>
     )
 }
