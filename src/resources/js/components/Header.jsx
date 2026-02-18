682786437de165df650898297fa5a86dafd435bb
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-02-18 17:22:33 +0800

    Header features display changes on mobile and desktop screen

--- src/resources/js/components/Header.jsx
+++ src/resources/js/components/Header.jsx
@@ -4,7 +4,7 @@ function Headers({ rightFeature }) {
 
     return (
         <div className="bg-black px-3 py-4 text-center d-flex flex-row justify-content-between">
-            <div>
+            <div className="mobile_header_feature">
                 {rightFeature?
                     (
                         <NavTab />
@@ -15,6 +15,9 @@ function Headers({ rightFeature }) {
             </div>
             <div className="text-center text-md-start fs-2 fw-bold">
                 Workout Tracker
+            </div>
+            <div className="desktop_header_feature">
+                Desktop Header Feature
             </div>
         </div>
     )
