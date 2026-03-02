e98102b823c8a2dff83891280dedf982d24c0765
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-03-02 19:05:27 +0800

    post form pop up by usestate

--- src/resources/js/forms/post.jsx
+++ src/resources/js/forms/post.jsx
@@ -1,10 +1,28 @@
+import { useState, useRef } from "react";
+
 function Post() {
+    const [postForm, setPostForm] = useState(false);
+    const postFormDiv = useRef(null);
 
     return (
         <>
-            <button className="btn btn-primary">
+            <button 
+                className="btn btn-primary"
+                onClick={() => setPostForm(!postForm)}
+            >
                 Create Post
             </button>
+            {postForm && (
+                <div
+                    ref={postFormDiv}
+                    className="position-fixed top-0 start-0 w-100 h-100 bg-dark bg-opacity-75"
+                >
+                    <form>
+                        Post Form
+                    </form>
+                </div>
+            )
+            }
         </>
     )
 }
