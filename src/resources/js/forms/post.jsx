763dd8ae4d70520c57b0f3843848c77f6c0ce984
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-03-02 21:44:41 +0800

    post form validation

--- src/resources/js/forms/post.jsx
+++ src/resources/js/forms/post.jsx
@@ -3,6 +3,32 @@ import { useState, useRef } from "react";
 function Post() {
     const [postForm, setPostForm] = useState(false);
     const postFormDiv = useRef(null);
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [errors, setErrors] = useState({});
+
+    function validateForm() {
+        const newErrors = {};
+
+        if (!title.trim()) {
+            newErrors.title = "Title is required!"
+        }
+
+        if (!content.trim()) {
+            newErrors.content = "At least a phrase is required..."
+        }
+
+        return newErrors;
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const validationErrors = validateForm();
+        setErrors(validationErrors);
+
+        if (Object.keys(validationErrors).length > 0) return;
+    }
 
     return (
         <>
@@ -28,20 +54,29 @@ function Post() {
                                 x
                             </button>
                         </div>
-                        <form className="w-75 bg-white rounded text-black d-flex flex-column align-items-center p-3">
+                        <form 
+                            onSubmit={handleSubmit}
+                            className="w-75 bg-white rounded text-black d-flex flex-column align-items-center p-3"
+                        >
                             Create a Post?
                             <div className="border mb-3 pt-1 pb-3 px-3 bg-secondary text-white rounded pt-3">
                                 <input
                                     type="text"
                                     className="form-control"
                                     placeholder="Title..."
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
                                 />
+                                {errors.title && <div className="text_sz text-light bg-danger rounded py-1 px-3 mt-1">{errors.title}</div>}
                             </div>
                             <div className="border mb-3 pt-1 pb-3 px-3 bg-secondary text-white rounded pt-3 w-100">
                                 <textarea
                                     className="text_area p-1 rounded w-100"
                                     placeholder="Enter a post..."
+                                    value={content}
+                                    onChange={(e) => setContent(e.target.value)}
                                 ></textarea>
+                                {errors.content && <div className="text_sz text-light bg-danger rounded py-1 px-3 mt-1">{errors.content}</div>}
                             </div>
                             <button
                                 type="submit"
