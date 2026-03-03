ce256e023343177e979307391ef6379ec31e1887
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-03-03 16:22:59 +0800

    relocated the login and register uri back to api...

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,10 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
 Route::middleware('auth:sanctum')->get('/users', [UserController::class, 'index']);
 Route::middleware('auth:sanctum')->post('/create-post', [PostController::class, 'createPost']);
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/register', [UserController::class, 'register']);
 
 // Catch-all for SPA
 Route::get('/{any}', function () {
