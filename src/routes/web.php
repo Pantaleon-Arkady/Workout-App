4ea290cc28eb35aa91d01bab5d2fa615020699f1
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-03-04 19:58:14 +0800

    Web file for routes:   - compiled all url/uri in the web file since auth guard is set/tied to web, which is the default   - compiled all url/uri because in web, all tokens and sessions cookies are authenticated to be used by front and backend...

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 
 // Catch-all for SPA
 Route::get('/{any}', function () {
     return view('app');
 })->where('any', '^(?!api).*$');
+
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout']);
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/create-post', [PostController::class, 'createPost']);
+    Route::get('/users', [UserController::class, 'index']);
+});
