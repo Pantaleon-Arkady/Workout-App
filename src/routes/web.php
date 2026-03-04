038fcb540cb8a3a6b6f3b1c47ae9425c7e60603b
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-03-04 20:32:10 +0800

    check auth url

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Auth;
 
 // Catch-all for SPA
 Route::get('/{any}', function () {
@@ -13,6 +14,14 @@ Route::get('/{any}', function () {
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout']);
+
+
+Route::get('/check-auth', function () {
+    return response()->json([
+        'authenticated' => Auth::check(),
+        'user' => Auth::user(),
+    ]);
+});
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
