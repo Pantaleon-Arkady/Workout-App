b419b7ff487f308cb125c837ebacd9df11f20ea0
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-03-04 20:01:15 +0800

    removed all url/uri until I figure out how to properly execute RestAPI * this is subject for development

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,10 +3,6 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
-
-Route::middleware('auth:sanctum')->get('/users', [UserController::class, 'index']);
-Route::middleware('auth:sanctum')->post('/create-post', [PostController::class, 'createPost']);
+// Protected routes
+Route::middleware('web')->post('/create-post', [PostController::class, 'createPost']);
 
