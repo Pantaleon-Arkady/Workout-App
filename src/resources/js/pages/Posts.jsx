eaa3c7eb1ca527e4a49a4cafce92bb6b595fb93a
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-03-05 12:03:00 +0800

    had to connect the create post with api uri...

--- src/resources/js/pages/Posts.jsx
+++ src/resources/js/pages/Posts.jsx
@@ -1,6 +1,34 @@
+import { useState, useEffect } from "react";
 import Headers from "../components/Header";
+import axios from "../../../axios";
 
 function Posts() {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    async function fetchPost() {
+        setLoading(true);
+
+        try {
+            const res = await axios.get("/api/retrieve-posts");
+
+            console.log("API reponse:", res);
+
+            const json = res.data;
+
+            console.log("JSON:", json);
+
+            setPosts(Array.isArray(json.data) ? json.data : [])
+        } catch (err) {
+            console.error("Fetch failed:", err)
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchPost();
+    }, []);
 
     return (
         <div className="d-flex flex-column text-white vh-100">
@@ -8,7 +36,15 @@ function Posts() {
                 rightFeature={true}
                 page="post"
             />
-            Posts page
+            {loading && <span>Loading Posts...</span>}
+
+            {!loading && posts.length == 0 && (
+                <div className="text-white">No posts retrieved</div>
+            )}
+
+            {!loading && posts.length > 0 && (
+                <div className="text-white">Posts retrieved: {posts.length}</div>
+            )}
         </div>
     )
 }
