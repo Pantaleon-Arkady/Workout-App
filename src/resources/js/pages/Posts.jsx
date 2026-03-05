59e707bdbedd13f5feec91830e99f7ecfa3f5d89
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-03-05 14:23:01 +0800

    Basic display logic for posts queried

--- src/resources/js/pages/Posts.jsx
+++ src/resources/js/pages/Posts.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
 import Headers from "../components/Header";
 import axios from "../../../axios";
+import GreetingsDiv from "../components/Greetings";
+import { useAuth } from "../context/AuthContext";
 
 function Posts() {
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
+
+    const { user } = useAuth();
 
     async function fetchPost() {
         setLoading(true);
@@ -43,7 +47,32 @@ function Posts() {
             )}
 
             {!loading && posts.length > 0 && (
-                <div className="text-white">Posts retrieved: {posts.length}</div>
+                <div className="d-flex homepage_main_div">
+                    <div className="desktop_side_tab border w-25">
+                        <GreetingsDiv
+                            username={user.name}
+                        />
+                    </div>
+                    <div className="border p-1 home_content" >
+                        {/* <div className="text-white">
+                            Posts retrieved: {posts.length}
+                        </div> */}
+                        {posts.map((post) => (
+                                <div key={post.id} className="border pt-2 rounded p-1 mb-3">
+                                    <div className="d-flex flex-row justify-content-between reg_fs bor">
+                                        <span>{post.user.name}</span>
+                                        <span>
+                                            {new Date(post.created_at).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} ~ {new Date(post.created_at).toLocaleDateString()} {" "}
+                                        </span>
+                                    </div>
+                                    <hr></hr>
+                                    <div className="">
+                                        {post.content}
+                                    </div>
+                                </div>
+                        ))}
+                    </div>
+                </div>
             )}
         </div>
     )
