10d50d95f07664c2788a45f25cb0f230f966702f
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-03-12 12:01:15 +0800

    plus button sign styles

--- src/resources/js/components/Tracker.jsx
+++ src/resources/js/components/Tracker.jsx
@@ -2,13 +2,20 @@ function Tracker() {
 
     return (
         <div className="progress_visual_div border rounded w-100 d-flex flex-row flex-wrap px-1 pt-2">
+            <div className="each_day_week border">
+                <div className="border text-center">
+                    Sunday
+                </div>
+                <button className="add_day_btn">
+                    <img src="/statics/plus.svg" className="plus_svg"/> Add
+                </button>
+            </div>
             <div className="each_day_week border">Monday</div>
             <div className="each_day_week border">Tuesday</div>
             <div className="each_day_week border">Wednesday</div>
             <div className="each_day_week border">Thursday</div>
             <div className="each_day_week border">Friday</div>
             <div className="each_day_week border">Saturday</div>
-            <div className="each_day_week border">Sunday</div>
         </div>
     )
 }
