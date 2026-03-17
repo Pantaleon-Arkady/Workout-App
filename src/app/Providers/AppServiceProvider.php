fafb279ce1c869b4e498921953558f22b97023ad
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-03-17 10:34:08 +0800

    log display of sql in terminal

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        DB::listen(function ($query) {
+            file_put_contents(
+                'php://stdout',
+                $query->sql . PHP_EOL
+            );
+        });
     }
 }
