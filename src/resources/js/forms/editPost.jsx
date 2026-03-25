c14e9c01480cd8360edf968a0c8dc359d0a4604c
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-03-25 13:39:21 +0800

    request edit

--- src/resources/js/forms/editPost.jsx
+++ src/resources/js/forms/editPost.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "../../../axios";
+import { useNavigate } from "react-router-dom";
 
 function EditPost() {
     const { id } = useParams();
+    const navigate = useNavigate();
 
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
-
+ 
     useEffect(() => {
         axios.get(`/api/posts/${id}`)
             .then(res => {
@@ -16,15 +18,24 @@ function EditPost() {
             })
     }, [id]);
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        await axios.put(`/api/posts/${id}`, {
+            title,
+            content
+        });
+
+        navigate(`/post${location.search}`);
+    };
+
     return (
-        <>
-            <div className="text-white">
-                Editing Post {id}<br/>
-                Title: {title}<br/>
-                Content: {content}
-            </div>
-        </>
-    )
+        <form onSubmit={handleSubmit}>
+            <input value={title} onChange={e => setTitle(e.target.value)} />
+            <textarea value={content} onChange={e => setContent(e.target.value)} />
+            <button type="submit">Save</button>
+        </form>
+    );
 }
 
 export default EditPost;
